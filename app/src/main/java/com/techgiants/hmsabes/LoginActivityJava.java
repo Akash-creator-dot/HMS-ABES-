e6b60362d22ecb2629951820c8a6d374dc8f0793
Deepansh Gupta <deepansh.gup124@gmail.com>
2024-07-18 20:09:48 +0530

    Deepansh

--- app/src/main/java/com/techgiants/hmsabes/LoginActivityJava.java
+++ app/src/main/java/com/techgiants/hmsabes/LoginActivityJava.java
@@ -1,15 +1,21 @@
 package com.techgiants.hmsabes;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,6 +27,7 @@ public class LoginActivityJava extends AppCompatActivity {
     private ActivityLoginBinding binding;
     private FirebaseAuth auth;
     private ProgressBar progressBar;
+    private TextView forgotTextLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,7 @@ public class LoginActivityJava extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         progressBar = binding.progressBarLogin;
         progressBar.setVisibility(View.GONE);
+        forgotTextLink=findViewById(R.id.forgotPasswordText);
 
         // Check if user already logged in
         FirebaseUser currentUser = auth.getCurrentUser();
@@ -78,6 +86,7 @@ public class LoginActivityJava extends AppCompatActivity {
         });
     }
 
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -87,5 +96,41 @@ public class LoginActivityJava extends AppCompatActivity {
             startActivity(new Intent(this, MainActivity.class));
             finish();
         }
-    }
+        forgotTextLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText resetMail=new EditText(v.getContext());
+                AlertDialog.Builder passwordResetDialog=new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password?");
+                passwordResetDialog.setMessage("Enter your email to receive reset password link.");
+                passwordResetDialog.setView(resetMail);
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //extract the mail and set reset link
+                        String mail =resetMail.getText().toString();
+                        auth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(LoginActivityJava.this, "Reset link sent to your mail.", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivityJava.this, "Error! Reset link not sent."+e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //close the dialog
+                    }
+                });
+                passwordResetDialog.create().show();
+            }
+        });
+        };
+
 }
