c916b8437af16cc8c3c4a3e30687e9dd8e6ff1b9
Deepansh Gupta <deepansh.gup124@gmail.com>
2024-07-26 17:30:36 +0530

    Deepansh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,10 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.lottie)
     implementation(libs.firebase.messaging)
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.activity:activity:1.9.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     androidTestImplementation(libs.ext.junit)
     implementation (libs.play.services.auth) {
         exclude ("com.google.android.gms", "play-services-auth")
---
+++ app/src/main/java/com/techgiants/hmsabes/PinAndBiometric.java
@@ -0,0 +1,4 @@
+package com.techgiants.hmsabes;
+
+public class PinAndBiometric {
+}
